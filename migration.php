b63d3484866f9f9deedae98e49ef9d9caa3942c2
Fabrice <aigleblanc@gmail.com>
2015-12-21 16:53:23 +0100

    ajout de diverses options de migration

--- migration.php
+++ migration.php
@@ -1,6 +1,9 @@
 <?php
 
-$retour = FALSE;
+$retour 			= FALSE;
+$retour_export 		= FALSE;
+$retour_import 		= FALSE;
+$retour_htaccess 	= FALSE;
 
 include ('wp-config.php');
 
@@ -64,6 +67,51 @@ if(isset($_POST['old']) && isset($_POST['new'])) {
 	}
 }
 
+if(isset($_POST['htaccess'])) {
+
+	if(!empty($_POST['htaccess'])) {
+
+		$path = dirname($_SERVER['REQUEST_URI']);
+
+		/*
+		<IfModule mod_rewrite.c>
+		RewriteEngine On
+		RewriteBase /projets/mont-saleve2015/
+		RewriteRule ^index\.php$ - [L]
+		RewriteCond %{REQUEST_FILENAME} !-f
+		RewriteCond %{REQUEST_FILENAME} !-d
+		RewriteRule . /projets/mont-saleve2015/index.php [L]
+		</IfModule>
+		*/
+	}
+}
+
+if(isset($_POST['exporter'])) {
+
+	if(!empty($_POST['exporter'])) {
+
+		//-- Dump SQL
+		exec('mysqldump --host='.DB_HOST.' --user='.DB_USER.' --password='.DB_PASSWORD.' '.DB_NAME.' > export.sql');
+
+		//-- Zip Files
+		Zip('./', "dumpFILES.zip");
+
+		$retour_export = TRUE;
+	}
+}
+
+if(isset($_POST['importer'])) {
+
+	if(!empty($_POST['importer'])) {
+
+	    $zip = new ZipArchive;
+	    $zip->open('dumpFILES.zip');
+	    $zip->extractTo('./');
+	    $zip->close();
+
+		$retour_import = TRUE;
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -102,6 +150,20 @@ if(isset($_POST['old']) && isset($_POST['new'])) {
 
          <article class="row">
             <div class="col-md-12">
+				<div class="panel panel-warning">
+					<div class="panel-heading"> 
+						<h3 class="panel-title">Important</h3> 
+					</div>
+					<div class="panel-body">
+					Pensez a supprimer le fichier migration.php de votre installation Wordpress apres avoir effectuer le changement des URLs.
+					</div>
+				</div>
+			</div>
+		</article>
+
+         <article class="row">
+           <div class="col-md-12">
+                <h2>Ecriture des nouvelles Urls</h2>
 				<div class="panel panel-info">
 					<div class="panel-heading"> 
 						<h3 class="panel-title">Ce que fait cet assistant</h3> 
@@ -115,9 +177,6 @@ if(isset($_POST['old']) && isset($_POST['new'])) {
 					</div>
 				</div>
 			</div>
-		</article>
-
-         <article class="row">
             <div class="col-md-12">
             	<?php if($retour == TRUE) : ?>
             		<div class="alert alert-success" role="alert">L'ecriture des nouvelles urls a bien ete effectue dans la base de données.</div>
@@ -138,18 +197,131 @@ if(isset($_POST['old']) && isset($_POST['new'])) {
             </div>
         </article>
 
-         <article class="row">
-            <div class="col-md-12">
-				<div class="panel panel-warning">
+        <article class="row">
+           	<div class="col-md-12">
+           	    <h2>Creation d'une archive avec les fichiers de Wordpress et le SQL</h2>
+				<div class="panel panel-info">
 					<div class="panel-heading"> 
-						<h3 class="panel-title">Important</h3> 
+						<h3 class="panel-title">Ce que fait cet assistant</h3> 
 					</div>
 					<div class="panel-body">
-					Pensez a supprimer le fichier migration.php de votre installation Wordpress apres avoir effectuer le changement des URLs.
+					    <ul>
+					    	<li>Extrait la base données si possible ( depend des serveurs )</li>
+					    	<li>Creer un Zip de tout le repertoire courant avec le SQL dedans</li>
+					    </ul>
 					</div>
 				</div>
 			</div>
-		</article>
+
+            <div class="col-md-12">
+                <?php if($retour_export == TRUE) : ?>
+            		<div class="alert alert-success" role="alert">
+            		L'export a ete effectue avec succes.
+            			<p><a href="/dumpFILES.zip">Telecharger le Dump des Fichers</a></p>
+            		</div>
+            	<?php endif; ?>
+				<form method="post">
+					<div class="form-group">
+						<input type="hidden" class="form-control" id="exporter" name="exporter" placeholder="" value="test">
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-default">Creer le Zip des fichiers</button>
+					</div>
+				</form>
+
+				<?php if(is_file('dumpFILES.zip') && $retour_export == FALSE) : ?>
+				<div class="panel panel-success">
+					<div class="panel-heading"> 
+						<h3 class="panel-title">Information</h3> 
+					</div>
+					<div class="panel-body">
+					    <ul>
+					    	<li>Un Dump existe deja, telecharger l'existant ? <a href="/dumpFILES.zip">Telecharger le Dump existant</a></li>
+					    </ul>
+					</div>
+				</div>
+				<?php endif; ?>
+
+            </div>
+        </article>
+
+        <article class="row">
+           	<div class="col-md-12">
+           	    <h2>Extraction des fichiers de Wordpress</h2>
+				<div class="panel panel-info">
+					<div class="panel-heading"> 
+						<h3 class="panel-title">Ce que fait cet assistant</h3> 
+					</div>
+					<div class="panel-body">
+					    <ul>
+					    	<li>Permet d'extraire le fichier dumpé precedement</li>
+					    	<li>Copier/Coller le fichier Zip sans le renommer</li>
+					    </ul>
+					</div>
+				</div>
+			</div>
+
+            <div class="col-md-12">
+                <?php if($retour_import == TRUE) : ?>
+            		<div class="alert alert-success" role="alert">L'extraction des fichiers a ete effectue avec succes.</div>
+            	<?php endif; ?>
+				<form method="post">
+					<div class="form-group">
+						<input type="hidden" class="form-control" id="importer" name="importer" placeholder="" value="test">
+					</div>
+					<?php if(is_file('dumpFILES.zip')): ?>
+					<div class="form-group">
+						<button type="submit" class="btn btn-default">Extraire les fichiers dans le dossier courant</button>
+					</div>
+					<?php endif; ?>
+				</form>
+
+				<?php if(!is_file('dumpFILES.zip')): ?>
+				<div class="panel panel-warning">
+					<div class="panel-heading"> 
+						<h3 class="panel-title">Information</h3> 
+					</div>
+					<div class="panel-body">
+					    <ul>
+					    	<li>Le fichier dumpFILES.zip n'est pas present sur le serveur</li>
+					    </ul>
+					</div>
+				</div>
+				<?php endif; ?>
+
+            </div>
+        </article>
+
+        <article class="row">
+           	<div class="col-md-12">
+           	    <h2>Creer le fichier HTACCESS wordpress</h2>
+				<div class="panel panel-info">
+					<div class="panel-heading"> 
+						<h3 class="panel-title">Ce que fait cet assistant</h3> 
+					</div>
+					<div class="panel-body">
+					    <ul>
+					    	<li></li>
+					    	<li></li>
+					    </ul>
+					</div>
+				</div>
+			</div>
+
+            <div class="col-md-12">
+                <?php if($retour_htaccess == TRUE) : ?>
+            		<div class="alert alert-success" role="alert">L'extraction des fichiers a ete effectue avec succes.</div>
+            	<?php endif; ?>
+				<form method="post">
+					<div class="form-group">
+						<input type="hidden" class="form-control" id="htaccess" name="htaccess" placeholder="" value="test">
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-default">Creer le fichier HTaccess</button>
+					</div>
+				</form>
+            </div>
+        </article>
 
         <footer class="row">
             <div class="col-md-12"></div>
@@ -161,3 +333,52 @@ if(isset($_POST['old']) && isset($_POST['new'])) {
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
    </body>
 </html>
+
+<?php 
+
+function Zip($source, $destination)
+{
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
+    }
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true)
+    {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file)
+        {
+            $file = str_replace('\\', '/', $file);
+
+            // Ignore "." and ".." folders
+            if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                continue;
+
+            $file = realpath($file);
+
+            if (is_dir($file) === true)
+            {
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            }
+            else if (is_file($file) === true)
+            {
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    }
+    else if (is_file($source) === true)
+    {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+
+    return $zip->close();
+}
+
+?>
